3c0e221c87fe917d9cbbab91a5a5e74f58d55d62
librae <librae8226@gmail.com>
2011-12-02 17:36:06 +0800

    update affinity_dev

--- affinity_dev/afftool.sh
+++ affinity_dev/afftool.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-IRQ_BAK_PATH=/home/yliao/Desktop/affinity_dev/irq
-IRQ_PROC_PATH=/home/yliao/Desktop/affinity_dev/irq_test
-PS_BAK_PATH=/home/yliao/Desktop/affinity_dev/ps
-SCHEDTOOL_PATH=/home/yliao/Desktop/affinity_dev
+# NOTE!
+# This script is used for suspend & resume mechanism.
+# All critial and miscellaneous stuffs could backup and restore in this script
+# Add more requirements by provide functions as backup_xxx() and restore_xxx().
+
+##### customization ######
+ENV_BAK_PATH=/home/yliao/Desktop/affinity_dev/suspend.bak
+IRQ_BAK_PATH=$ENV_BAK_PATH/irq
+PS_BAK_PATH=$ENV_BAK_PATH/ps
+IRQ_PROC_PATH=/home/yliao/Desktop/affinity_dev/irq
+SCHEDTOOL_PATH=/system/bin
+TOOL_PREFIX=
 
 backup_irq()
 {
-	cd /proc/irq
+	mkdir -p $IRQ_BAK_PATH
+	$TOOL_PREFIX touch $IRQ_BAK_PATH/irq_affinity
+	cd $IRQ_PROC_PATH
 	for name in `ls`
 	do
-		test -d $name
-		if [ $? == "0" ]; then
-			cd $name
-			pwd
-			mkdir -p $IRQ_BAK_PATH/$name
-			touch $IRQ_BAK_PATH/$name/smp_affinity
-			cat ./smp_affinity > $IRQ_BAK_PATH/$name/smp_affinity
-			cd ..
-		fi
+		#$TOOL_PREFIX printf "%3d %d\n" $name `cat $name/smp_affinity 2>/dev/null` >> $IRQ_BAK_PATH/irq_affinity
+		/home/yliao/Desktop/affinity_dev/fileopt/fileopt $IRQ_PROC_PATH/$name >> $IRQ_BAK_PATH/irq_affinity
 	done
 	cd - >/dev/null
 }
@@ -26,54 +29,62 @@ backup_irq()
 restore_irq()
 {
 	cd $IRQ_BAK_PATH
-	for name in `ls`
+	while read line
 	do
-		cd $name
-		pwd
-		cat ./smp_affinity > $IRQ_PROC_PATH/$name/smp_affinity
-		cd ..
-	done
+		echo "$line" > .irq_restore_tmp
+		name=`$TOOL_PREFIX awk '{print $1}'` < .irq_restore_tmp
+		aff=`$TOOL_PREFIX awk '{print $2}'` < .irq_restore_tmp
+		echo $aff > $IRQ_PROC_PATH/$name/smp_affinity
+	done < irq_affinity
 	cd - >/dev/null
 }
 
 backup_ps()
 {
+	mkdir -p $PS_BAK_PATH
+	$TOOL_PREFIX touch $PS_BAK_PATH/ps_affinity
+	$TOOL_PREFIX touch $PS_BAK_PATH/.ps_backup_tmp
 	cd /proc
 	for name in `ls`
 	do
-		#if [[ $name =~ ^[0-9]*.?[0-9]*$ ]]; then     
-		#	echo "$name is numeric !!!"
-		#else
-		#	echo "$name is NOT numeric"
-		#fi
-		expr $name : '^[0-9]\+$' >/dev/null 2>&1
+		$TOOL_PREFIX expr $name : '^[0-9]\+$' >/dev/null 2>&1
 		if [ $? -eq 0 ]; then
-			aff=`$SCHEDTOOL_PATH/schedtool $name | awk -F , '{print $4}' | awk -F " " '{print $2}'`
-			if [ "$aff" ]; then
-				mkdir -p $PS_BAK_PATH/$name
-				touch $PS_BAK_PATH/$name/ps_affinity
-				echo $aff > $PS_BAK_PATH/$name/ps_affinity
-			else
-				echo "NULL string"
-			fi
+			echo "$name" >> $PS_BAK_PATH/.ps_backup_tmp
 		fi
 	done
+	pid=`cat $PS_BAK_PATH/.ps_backup_tmp`
+	$SCHEDTOOL_PATH/schedtool $pid > $PS_BAK_PATH/ps_affinity
 	cd - >/dev/null
 }
 
 restore_ps()
 {
 	cd $PS_BAK_PATH
-	for name in `ls`
-	do
-		$SCHEDTOOL_PATH/schedtool -a `cat $name/ps_affinity` $name
-	done
+	$SCHEDTOOL_PATH/schedtool -z $PS_BAK_PATH/ps_affinity >/dev/null 2>&1
 	cd - >/dev/null
 }
 
-echo '[backup begin]'
-backup_ps
-echo '[backup done]'
-echo '[restore begin]'
-restore_ps
-echo '[restore done]'
+main()
+{
+	if [ -n "$1" ]; then
+		if [ $1 == "backup" ]; then
+			rm -rf $ENV_BAK_PATH
+			backup_irq
+			#backup_ps
+			:
+		elif [ $1 == "restore" ]; then
+			#restore_irq
+			#restore_ps
+			:
+		else
+			echo "USAGE: ./afftool.sh ARG"
+			echo "ARG could be: backup OR restore."
+		fi
+
+	else
+		echo "USAGE: ./afftool.sh ARG"
+		echo "ARG could be: backup OR restore."
+	fi
+}
+
+main "$@"
--- affinity_dev/fileopt/fileopt.c
+++ affinity_dev/fileopt/fileopt.c
@@ -2,22 +2,35 @@
 #include <stdio.h>
 #include <string.h>
 
+#define VIRT_PROC_IRQ_DIR	"/home/yliao/Desktop/affinity_dev/irq"
+
+int ps_affinity(char *filepath);
+int irq_affinity(char *filepath);
 unsigned long parse_affinity(char *arg);
+int irq_set_affinity(const char *irq_dir, int iid, int aff);
+int print_irq_affinity(const char *irq_dir);
 
 int main(int argc, char *argv[])
 {
-	FILE *fp = NULL;
 	char *filepath = NULL;
+
+	filepath = argv[1];
+	print_irq_affinity(filepath);
+
+	return 0;
+}
+
+int ps_affinity(char *filepath)
+{
+	FILE *fp = NULL;
 	char ch = 0;
 	char arr_pid[5] = {0};
 	char arr_aff[3] = {0};
 	pid_t pid = 0;
-	unsigned long aff = 0;
+	unsigned long int aff = 0;
 
-	filepath = argv[1];
-	printf("file path: %s\n",filepath); 
 	if (filepath == NULL) {
-		printf("please specify a file!\n");
+		printf("please specify ps_affinity file!\n");
 		return -1;
 	}
 	if ((fp = fopen(filepath, "r")) == NULL) {
@@ -25,25 +38,60 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 	fseek(fp, 0L, SEEK_SET);
-	while(1) {
+	while (1) {
 		fgets(arr_pid, 6, fp);
 		pid = (pid_t)atoi(arr_pid);
 		if (pid == 0) {
 			fprintf(stderr, "get pid failed!\n");
 		} else {
-			printf("%d", pid);
 			fseek(fp, 1L, SEEK_CUR);
 			fgets(arr_aff, 4, fp);
-			printf(": %s\n", arr_aff);
-			/* TODO process data */
-			printf(">>> pid: %d, aff: %lu\n", pid, parse_affinity(arr_aff));
+			aff = parse_affinity(arr_aff);
 			if (fgetc(fp) == EOF)
 				break;
+			printf("%d: %lu\n", pid, aff);
+			/* TODO process data */
 		}
 	}
 
 	fclose(fp);
-	return 0;
+}
+
+int irq_affinity(char *filepath)
+{
+	FILE *fp = NULL;
+	char ch = 0;
+	char irq_id[3] = {0};
+	char irq_aff = 0;
+	int iid = 0;
+	int aff = 0;
+	int ret = 0;
+
+	if (filepath == NULL) {
+		printf("please specify irq_affinity file!\n");
+		return -1;
+	}
+	if ((fp = fopen(filepath, "r")) == NULL) {
+		printf("`%s' open failed!\n", filepath);
+		return -1;
+	}
+	fseek(fp, 0L, SEEK_SET);
+	while (1) {
+		fgets(irq_id, 4, fp);
+		iid = atoi(irq_id);
+		fseek(fp, 1L, SEEK_CUR);
+		irq_aff = fgetc(fp);
+		aff = atoi(&irq_aff);
+		if (fgetc(fp) == EOF)
+			break;
+		printf("%d: %d\n", iid, aff);
+		/* TODO process data */
+		ret = irq_set_affinity(VIRT_PROC_IRQ_DIR, iid, aff);
+		if (ret != 0)
+			printf("irq_set_affinity failed!\n");
+	}
+
+	fclose(fp);
 }
 
 unsigned long parse_affinity(char *arg)
@@ -74,4 +122,74 @@ unsigned long parse_affinity(char *arg)
 		exit(1);
 	}
 	return tmp_aff;
+}
+
+int irq_set_affinity(const char *irq_dir, int iid, int aff)
+{
+	char filepath[50] = {0};
+	char iid_str[3] = {0};
+	char aff_str[2] = {0};
+	FILE *fp = NULL;
+
+	/* WARN abort to check string length */
+	strcpy(filepath, irq_dir);
+	sprintf(iid_str, "%d", iid);
+	strcat(filepath, "/");
+	strcat(filepath, iid_str);
+	strcat(filepath, "/");
+	strcat(filepath, "smp_affinity");
+
+	if (filepath == NULL) {
+		printf("please specify irq smp_affinity file!\n");
+		return -1;
+	}
+	if ((fp = fopen(filepath, "wb")) == NULL) {
+		printf("`%s' open failed!\n", filepath);
+		return -1;
+	}
+	fseek(fp, 0L, SEEK_SET);
+	sprintf(aff_str, "%d\n", aff);
+	fputs(aff_str, fp);
+
+	fclose(fp);
+	return 0;
+}
+
+int print_irq_affinity(const char *irq_dir)
+{
+	char filepath[100] = {0};
+	char aff_str[2] = {0};
+	char *pstr = NULL;
+	char *pstr_iid = NULL;
+	int iid = 0;
+	FILE *fp = NULL;
+
+	/* WARN abort to check string length */
+	strcpy(filepath, irq_dir);
+	pstr = filepath;
+	while (*pstr != '\0') {
+		if (*pstr == '/' && *(pstr + 1) != '\0')
+			pstr_iid = pstr + 1;
+		pstr++;
+	}
+	if (pstr_iid == NULL || *pstr_iid == 'd')
+		return -1;
+	iid = atoi(pstr_iid);
+	strcat(filepath, "/");
+	strcat(filepath, "smp_affinity");
+
+	if (filepath == NULL) {
+		printf("please specify irq smp_affinity file!\n");
+		return -1;
+	}
+	if ((fp = fopen(filepath, "r")) == NULL) {
+		printf("`%s' open failed!\n", filepath);
+		return -1;
+	}
+	fseek(fp, 0L, SEEK_SET);
+	fgets(aff_str, 2, fp);
+	printf("%3d %s\n", iid, aff_str);
+
+	fclose(fp);
+	return 0;
 }
