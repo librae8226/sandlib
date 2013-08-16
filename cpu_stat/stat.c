218ffdac7d001b01d680f8dfc3c97d21415d284e
Librae <librae8226@gmail.com>
2013-08-16 12:02:00 +0800

    update cpu_stat

--- cpu_stat/stat.c
+++ cpu_stat/stat.c
@@ -16,7 +16,7 @@
 
 
 #define FTIME_IN_STATE	"time_in_state"
-#define NSTATE		6
+#define NSTATE_MAX	32
 
 struct time_in_state {
 	int state;
@@ -52,18 +52,41 @@ static int file_read(char *fname, char *pdata)
 
 static int fill_tis(struct time_in_state *tis, char *pbuf)
 {
+	char tbuf[16] = {0};
+	char sbuf[16] = {0};
+	char *p = NULL;
+
+	assert(pbuf != NULL);
+	assert(tis != NULL);
+
 	printf("0x%p: ", tis);
 	fputs(pbuf, stdout);
+
+	p = strrchr(pbuf, ' ');
+	assert(p != NULL);
+
+	strcpy(tbuf, p + 1);
+	strncpy(sbuf, pbuf, p - pbuf);
+
+	tis->state = atoi(sbuf);
+	tis->time = atoi(tbuf);
+
+	printf("s: %d\n", tis->state);
+	printf("t: %d\n", tis->time);
+
 	return 0;
 }
 
-static int fill_tis_table(char *fname, struct time_in_state *tbl, char *buf)
+static int fill_tis_table(char *fname, struct time_in_state tbl[], char *buf)
 {
 	FILE *fp = NULL;
 	char *pbuf = NULL;
 	int sz = 0;
 	int ret = 0;
 	int i = 0;
+
+	assert(tbl != NULL);
+	assert(buf != NULL);
 
 	fp = fopen(fname, "r");
 	if (!fp) {
@@ -76,16 +99,9 @@ static int fill_tis_table(char *fname, struct time_in_state *tbl, char *buf)
 	rewind(fp);
 
 	do {
-		if (i >= NSTATE) {
-			printf("tis table full.\n");
-			break;
-		}
 		pbuf = fgets(buf, sz, fp);
-		if (pbuf == NULL) {
-			printf("read failed.\n");
-			fclose(fp);
-			return -EIO;
-		}
+		if (pbuf == NULL)
+			break;
 
 		ret = fill_tis(&tbl[i], pbuf);
 		if (ret < 0) {
@@ -96,13 +112,34 @@ static int fill_tis_table(char *fname, struct time_in_state *tbl, char *buf)
 		i++;
 	} while (pbuf != NULL);
 
+	printf("nstate: %d\n", i);
+
 	fclose(fp);
-	return ret;
+	return i;
 }
 
-static int get_cpu_duty(struct time_in_state *tbl)
+static unsigned int get_cpu_duty(struct time_in_state tbl[], int nstate)
 {
-	return 0;
+	int smax = 0;
+	int tmax = 0;
+	int idmax = 0;
+	int time_sum = 0;
+	int i;
+	unsigned int duty;
+
+	for (i = 0; i < nstate; i++) {
+		if (smax < tbl[i].state) {
+			smax = tbl[i].state;
+			idmax = i;
+		}
+		time_sum += tbl[i].time;
+	}
+
+	tmax = tbl[idmax].time;
+	printf("id: %d, smax: %d, tmax: %d\n", idmax, smax, tmax);
+	duty = (unsigned int)tmax * 100 / time_sum;
+
+	return duty;
 }
 
 int main(int argc, char *argv[])
@@ -110,14 +147,17 @@ int main(int argc, char *argv[])
 	int ret;
 	char buf[256] = {0};
 	int duty = 0;
-	struct time_in_state tis_table[NSTATE];
+	int nstate = 0;
+	struct time_in_state tis_table[NSTATE_MAX];
 
 	ret = file_read(FTIME_IN_STATE, buf);
 	printf("ret %d\n", ret);
+	printf("raw data:\n");
 	fprintf(stdout, buf, 256);
 
-	fill_tis_table(FTIME_IN_STATE, tis_table, buf);
-	duty = get_cpu_duty(tis_table);
+	printf("process:\n");
+	nstate = fill_tis_table(FTIME_IN_STATE, tis_table, buf);
+	duty = get_cpu_duty(tis_table, nstate);
 
 	printf("duty: %d\n", duty);
 
