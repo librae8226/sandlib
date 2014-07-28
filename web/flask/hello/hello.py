d6fa04a36ae14136d0295c651387749d5395bcab
Librae <librae8226@gmail.com>
2014-07-28 16:20:55 +0800

    update flash sample

--- web/flask/hello/hello.py
+++ web/flask/hello/hello.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, request, url_for, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	    return 'Index Page'
 
-@app.route('/hello')
-def hello():
-	return 'Hello World'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+	return render_template('hello.html', name=name)
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -26,6 +28,32 @@ def projects():
 @app.route('/about')
 def about():
 	return 'The about page'
+
+@app.route('/func/<func>')
+def run_func(func):
+	return 'func name: %s' % func
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	print 'login entry'
+	if request.method == 'POST':
+		print 'calling do_the_login'
+		do_the_login()
+	else:
+		print 'calling show_the_login_form'
+		show_the_login_form()
+	return 'login process done'
+
+def do_the_login():
+	"""doc for do_the_login"""
+	print 'do_the_login'
+
+def show_the_login_form():
+	"""doc for show_the_login_form"""
+	print 'show_the_login_form'
+
+with app.test_request_context():
+	print url_for('static', filename='style.css')
 
 if __name__ == '__main__':
 	app.run(debug=True)
