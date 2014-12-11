450a3c9ae100e0c1bd558f99243730b83150412f
Librae <librae8226@gmail.com>
2014-12-11 15:32:34 +0800

    get num of items at runtime

--- python/parse_json.py
+++ python/parse_json.py
@@ -5,27 +5,26 @@ import math
 import json
 from pprint import pprint
 
-# CONSTANTS
-NITEMS = 3
-
 json_data=open('public.json')
 
 data = json.load(json_data)
 pprint(data)
 
-capital = range(0, NITEMS)
-total = range(0, NITEMS)
+n = len(data)
 
-for i in range(0, NITEMS):
+capital = range(0, n)
+total = range(0, n)
+
+for i in range(0, n):
 	capital[i] = float(data[i]["capital"])
 	total[i] = float(data[i]["total"])
 
-for i in range(0, NITEMS):
+for i in range(0, n):
 	print '{0:s} - {1:8.2f} - {2:8.2f} - ({3:s})'.format(data[i]["timestamp"], capital[i], total[i], data[i]["info"])
 
-for i in range(0, NITEMS):
+for i in range(0, n):
 	sys.stdout.write("{0} # {1:>8} # {2:>8} # ({3})\n".format(data[i]["timestamp"], data[i]["capital"], data[i]["total"], data[i]["info"]))
 
 json_data.close()
 
-print 'The value of PI is approximately {0:.3f}.'.format(math.pi)
+print 'The value of PI is approximately {0:.4f}.'.format(math.pi)
