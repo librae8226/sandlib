53a4774b0fd20df4f0910107229acfb71801ead9
Librae <librae8226@gmail.com>
2015-05-28 22:06:59 +0800

    avoid bind fail

--- socket/server.c
+++ socket/server.c
@@ -35,9 +35,12 @@ int main(int argc, char *argv[])
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
 	serv_addr.sin_port = htons(portno);
+	int on = 1;
+	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 	if (bind(sockfd, (struct sockaddr *) &serv_addr,
-				sizeof(serv_addr)) < 0)
+				sizeof(serv_addr)) < 0) {
 		error("ERROR on binding");
+	}
 	listen(sockfd,5);
 	clilen = sizeof(cli_addr);
 	newsockfd = accept(sockfd,
