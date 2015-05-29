8cddef549bdabed1657bfa5fe1c955eccc56cbf5
Librae <librae8226@gmail.com>
2015-05-29 16:12:21 +0800

    update stress

--- shell/mqtt_stress.sh
+++ shell/mqtt_stress.sh
@@ -6,7 +6,7 @@ cnt=0
 
 #while [ $cnt -lt 256 ] ; do
 while true; do
-	mqtt_pub -h $host $topic "stress test #"$cnt
+	mqtt_pub -h $host $topic "`date '+%Y-%m-%d %k:%M:%S'` stress #"$cnt
 	echo "`expr $cnt`"
 	cnt=`expr $cnt + 1`
 	sleep 1
