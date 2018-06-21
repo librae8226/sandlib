541582222ce4a86cdf83fd2b9e8eb1a47fc9def7
librae8226 <librae8226@gmail.com>
2018-06-21 21:14:49 +0800

    update

--- node/node-lab/run-command.js
+++ node/node-lab/run-command.js
@@ -1,3 +1,48 @@
-const shellExec = require('shell-exec')
+'use strict'
 
-shellExec('echo Hi!').then(console.log).catch(console.log)
+const childProcess = require('child_process')
+
+function shellExec (cmd = '', opts = {}) {
+  if (Array.isArray(cmd)) {
+    cmd = cmd.join(';')
+  }
+
+  let child
+
+  try {
+    child = childProcess.spawn(cmd, opts)
+  } catch (error) {
+    return Promise.reject(error)
+  }
+
+  return new Promise(resolve => {
+    let stdout = ''
+    let stderr = ''
+
+    if (child.stdout) {
+      child.stdout.on('data', data => {
+        stdout += data
+      })
+    }
+
+    if (child.stderr) {
+      child.stderr.on('data', data => {
+        stderr += data
+      })
+    }
+
+    child.on('error', error => {
+      resolve({ error, stdout, stderr, cmd })
+    })
+
+    child.on('close', code => {
+      resolve({ stdout, stderr, cmd, code })
+    })
+  })
+}
+
+shellExec('aos', ['devices']).then((res) => {
+  console.log(JSON.parse(res.stdout))
+}).catch((err) => {
+  console.log(err)
+})
